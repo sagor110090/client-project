21de72ec567ee2bec6394e466a721a4ffb1bcc24
sagor110090 <hasan15-4933@diu.edu.bd>
2020-08-19 06:39:17 +0600

    register

--- signup-user.php
+++ signup-user.php
@@ -51,51 +51,171 @@ if (isset($_SESSION['success'])) {
     unset($_SESSION['success']);
 }
 ?>
-        <div class="container">
-            <div class="register-box-body">
-                <p class="login-box-msg"> Seller Register</p>
 
-                <form action="signup.php" method="POST">
-                    <div class="form-group has-feedback">
+        <style>
+        .feature-info-style-02 {
+            background: #ffffff;
+            padding: 35px;
+            overflow: hidden;
+            position: relative;
+            -webkit-box-shadow: 5px 5px 24px 0px rgba(2, 45, 98, 0.1);
+            box-shadow: 5px 5px 24px 0px rgba(2, 45, 98, 0.1);
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .feature-info-style-02 .feature-info-icon {
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            -webkit-box-align: center;
+            -ms-flex-align: center;
+            align-items: center;
+            margin-bottom: 15px;
+            position: inherit;
+            top: 0;
+        }
+
+        .section-title {
+            margin-bottom: 50px;
+        }
+
+        .section-title p {
+            /*font-size: 18px;
+        font-weight: 500; */
+            margin-bottom: 0;
+            line-height: 1.5;
+            text-align: justify;
+        }
+
+        .space-pt {
+            padding: 100px 0 0;
+        }
+
+        .ml-4,
+        .mx-4 {
+            margin-left: 1.5rem !important;
+        }
+
+        .bg-primary {
+            background-color: #18610f !important;
+        }
+
+
+
+        .card-body {
+
+            width: 60%;
+        }
+
+        .divider-text {
+            position: relative;
+            text-align: center;
+            margin-top: 15px;
+            margin-bottom: 15px;
+        }
+
+        .divider-text span {
+            padding: 7px;
+            font-size: 12px;
+            position: relative;
+            z-index: 2;
+        }
+
+        .divider-text:after {
+            content: "";
+            position: absolute;
+            width: 100%;
+            border-bottom: 1px solid #ddd;
+            top: 55%;
+            left: 0;
+            z-index: 1;
+        }
+
+        .btn-facebook {
+            background-color: #405D9D;
+            color: #fff;
+        }
+
+        .btn-twitter {
+            background-color: #42AEEC;
+            color: #fff;
+        }
+        </style>
+        <!-- Breadcrumb Area start -->
+
+        <section class="section-padding bg-dark inner-header"
+            style="background-image: url('images/Contact Us.jpg');height: 289px;">
+            <div class="container">
+                <div class="row">
+
+                </div>
+            </div>
+        </section>
+
+        <div class="card bg-light container">
+            <article class="card-body mx-auto" style=" margin:40px 0px">
+                <h4 class="card-title mt-3 text-center " style=" color:green">Create Customer Account</h4>
+                <p class="text-center mb-3">Get started with your free account</p>
+                <!--<p>
+		<a href="" class="btn btn-block btn-twitter"> <i class="fab fa-twitter"></i>   Login via Twitter</a>
+		<a href="" class="btn btn-block btn-facebook"> <i class="fab fa-facebook-f"></i>   Login via facebook</a>
+	</p>
+	<p class="divider-text">
+        <span class="bg-light">OR</span>
+    </p>-->
+                <form action="signup-user.php" method="POST">
+
+
+
+                    <div class="form-group input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+                        </div>
                         <input type="text" class="form-control" name="firstname" placeholder="Firstname"
                             value="<?php echo (isset($_SESSION['firstname'])) ? $_SESSION['firstname'] : '' ?>"
                             required>
-                        <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                    </div>
-                    <div class="form-group has-feedback">
+                    </div> <!-- form-group// -->
+                    <div class="form-group input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+                        </div>
                         <input type="text" class="form-control" name="lastname" placeholder="Lastname"
                             value="<?php echo (isset($_SESSION['lastname'])) ? $_SESSION['lastname'] : '' ?>" required>
-                        <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                    </div>
-                    <div class="form-group has-feedback">
+                    </div> <!-- form-group// -->
+
+                    <div class="form-group input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+                        </div>
                         <input type="email" class="form-control" name="email" placeholder="Email"
                             value="<?php echo (isset($_SESSION['email'])) ? $_SESSION['email'] : '' ?>" required>
-                        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                    </div>
-                    <div class="form-group has-feedback">
+                    </div> <!-- form-group// -->
+
+
+
+
+                    <div class="form-group input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+                        </div>
                         <input type="password" class="form-control" name="password" placeholder="Password" required>
-                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                    </div>
-                    <div class="form-group has-feedback">
+                    </div> <!-- form-group// -->
+                    <div class="form-group input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+                        </div>
                         <input type="password" class="form-control" name="repassword" placeholder="Retype password"
                             required>
-                        <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
-                    </div>
-
-                    <hr>
-                    <div class="row">
-                        <div class="col-xs-4">
-                            <button type="submit" class="btn btn-primary btn-block btn-flat" name="signup"><i
-                                    class="fa fa-pencil"></i> Sign Up</button>
-                        </div>
-                    </div>
+                    </div> <!-- form-group// -->
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat" name="signup"><i
+                                class="fa fa-pencil"></i> Sign Up</button>
+                    </div> <!-- form-group// -->
+                    <p class="text-center">Have an account? <a href="">Log In</a> </p>
                 </form>
-                <br>
-                <a href="login.php">I already have a membership</a><br>
-                <a href="index.php"><i class="fa fa-home"></i> Home</a>
-            </div>
-        </div>
-
+            </article>
+        </div> <!-- card.// -->
     </div>
 
     <?php include 'includes/scripts.php'?>
--- verify.php
+++ verify.php
@@ -1,50 +1,43 @@
 <?php
-	include 'includes/session.php';
-	$conn = $pdo->open();
+include 'includes/session.php';
+$conn = $pdo->open();
 
-	if(isset($_POST['login'])){
-		
-		$email = $_POST['email'];
-		$password = $_POST['password'];
+if (isset($_POST['login'])) {
 
-		try{
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-			$stmt = $conn->prepare("SELECT *, COUNT(*) AS numrows FROM users WHERE email = :email");
-			$stmt->execute(['email'=>$email]);
-			$row = $stmt->fetch();
-			if($row['numrows'] > 0){
-				if($row['status']){
-					if(password_verify($password, $row['password'])){
-						if($row['type']){
-							$_SESSION['admin'] = $row['id'];
-						}
-						else{
-							$_SESSION['user'] = $row['id'];
-						}
-					}
-					else{
-						$_SESSION['error'] = 'Incorrect Password';
-					}
-				}
-				else{
-					$_SESSION['error'] = 'Account not activated.';
-				}
-			}
-			else{
-				$_SESSION['error'] = 'Email not found';
-			}
-		}
-		catch(PDOException $e){
-			echo "There is some problem in connection: " . $e->getMessage();
-		}
+    try {
 
-	}
-	else{
-		$_SESSION['error'] = 'Input login credentails first';
-	}
+        $stmt = $conn->prepare("SELECT *, COUNT(*) AS numrows FROM users WHERE email = :email");
+        $stmt->execute(['email' => $email]);
+        $row = $stmt->fetch();
+        // die(print_r($row));
+        if ($row['numrows'] > 0) {
+            if ($row['status']) {
+                if ($password == $row['password']) {
+                    if ($row['type']) {
+                        $_SESSION['admin'] = $row['id'];
+                    } else {
+                        $_SESSION['user'] = $row['id'];
+                    }
+                } else {
+                    $_SESSION['error'] = 'Incorrect Password';
+                }
+            } else {
+                $_SESSION['error'] = 'Account not activated.';
+            }
+        } else {
+            $_SESSION['error'] = 'Email not found';
+        }
+    } catch (PDOException $e) {
+        echo "There is some problem in connection: " . $e->getMessage();
+    }
 
-	$pdo->close();
+} else {
+    $_SESSION['error'] = 'Input login credentails first';
+}
 
-	header('location: login.php');
+$pdo->close();
 
-?>
+header('location: login.php');
